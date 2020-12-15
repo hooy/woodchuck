76fb6b6d0ff09546cfe2e117292686b5350617a2
Sergey Gorbunov <info@maxwell.lv>
2020-12-15 15:06:16 +0200

    fix: state issues with woodchuck and subscription

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Hit } from './types';
 import './App.css';
 import io from 'socket.io-client'
@@ -7,26 +7,47 @@ import { Woodchuck } from './Woodchuck';
 const socket = io.connect(`http://localhost:5000`, {secure:false})
 
 const App: React.FC = () => {
-  let [hits, setHits] = useState([] as Hit[])
-  socket.on(`coords`, (hit: Hit) => {
-    console.log(hits);
-    setHits([...hits, hit])
+  const [hits, setHits] = useState([] as Hit[])
+  const hitsRef = React.useRef(hits)
+    
+  useEffect(() => {
+      // This effect executes on every render (no dependency array specified).
+      // Any change to the "hits" state will trigger a re-render
+      // which will then cause this effect to capture the current "hits"
+      // value in "hitsRef.current".
+      hitsRef.current = hits;
   });
 
-  {
-    return (
-      <div className="App">
-          <header className="App-header">
-              <h1 className="App-title">Woodchuck</h1>
-          </header>
-          <div className="App-content">
-              <div className="Woodchuck">
-               <Woodchuck hits={hits}/>
-              </div>
-          </div>
-      </div>
-    );
-  }
+  React.useEffect(() => {
+    // This effect only executes on the initial render so that we aren't setting
+    // up the socket repeatedly. This means it can't reliably refer to "hits"
+    // because once "setHits" is called this would be looking at a stale
+    // "hits" reference (it would forever see the initial value of the
+    // "hits" state since it isn't in the dependency array).
+    // "hitsRef", on the other hand, will be stable across re-renders and 
+    // "hitsRef.current" successfully provides the up-to-date value of 
+    // "hits" (due to the other effect updating the ref).
+    const handler = (hit: Hit) => {setHits([...hitsRef.current, hit])};
+    socket.on('coords', handler);
+    return () => {
+      socket.off('disconnect', handler);
+    }
+  }, []);
+
+  
+
+  return (
+    <div className="App">
+        <header className="App-header">
+            <h1 className="App-title">Woodchuck</h1>
+        </header>
+        <div className="App-content">
+            <div className="Woodchuck">
+              <Woodchuck hits={hits}/>
+            </div>
+        </div>
+    </div>
+  );
 };
 
 export default App;
--- client/src/Woodchuck.tsx
+++ client/src/Woodchuck.tsx
@@ -1,23 +1,24 @@
-import React, { FunctionComponent } from 'react';
+import React, { useEffect } from 'react';
 import { Layer, Stage, Image, Circle } from 'react-konva';
 import useImage from 'use-image';
 import { Hit, WoodchuckTarget } from './types';
 
 
-const WoodchuckImage = () => {
-    console.log('WoodchuckImage: ')
-    const woodchuckPath = require("./woodchuck.svg") as string
-    const [image] = useImage(woodchuckPath, 'Anonymous')
-    return <Image image={image} x={192} y={192} />
-}
-
+    
 export function Woodchuck({ hits }: WoodchuckTarget) {
+    const woodchuckPath = '/woodchuck.svg'
+    const [image, status] = useImage(woodchuckPath, 'Anonymous')
+    
+    // useEffect(() => {
+    //     console.log(woodchuckPath, image, status);
+    // }, [image, status]);
+
     return (
         <div>
             <div>
                 <Stage width={512} height={512}>
                     <Layer>
-                        <WoodchuckImage />
+                        <Image image={image} x={192} y={192} />
                         {hits.map((hit: Hit, index) => (
                             <Circle x={hit.x} y={hit.y} radius={5} fill="green" key={index} />
                         ))}
